699e429b37a393b4735dff263f77d3b0ae85f3a4
Sara <sgee03@gmail.com>
2016-06-25 14:34:15 -0700

    (feat) Initial addition of Graph, Progress, and Todo

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,64 +1,92 @@
 import _ from 'lodash'
 import { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Button, FormControl } from 'react-bootstrap';
 import Task from './Task'
+import ProgressGraph from './ProgressGraph'
+import InProgressList from './InProgressList'
+import ToDoList from './ToDoList'
 
 class Project extends Component {
-  render() {
-    const { addTask, startTask, id, name, tasks } = this.props
-    const taskDOM = (task) => {
-      return <Task
-              key={task.id}
-              startTask={startTask}
-              projectId={id}
-              {...task}
-            />
+
+  static propTypes = {
+    addTask: PropTypes.func.isRequired,
+    startTask: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    tasks: PropTypes.shape({
+      count: PropTypes.number.isRequired,
+      todo: PropTypes.number.isRequired,
+      inProgress: PropTypes.number.isRequired,
+      list: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        state: PropTypes.string.isRequired
+      }))
+    })
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = { task: '', description: '' }
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event) {
+    if (event.target.id === 'task') {
+      this.setState({ task: event.target.value });
+    }  
+    if (event.target.id === 'description') {
+      this.setState({ description: event.target.value });
     }
-    let todoList = _.filter(tasks.list, t => t.state === 'to-do').map(taskDOM)
-    let inProgressList = _.filter(tasks.list, t => t.state === 'in-progress').map(taskDOM)
-    let input
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    this.props.addTask(this.props.id, this.state.task, this.state.description);
+    this.setState({ task: '' });
+    this.setState({ description: '' });
+  }
+
+
+  render() {
     return (
       <div>
-        <h3>{name}</h3>
+        <ProgressGraph {...this.props} />
         <div>
-          <p>Tasks: {tasks.count}, To Do: {tasks.todo}, In Progress: {tasks.inProgress}</p>
-        </div>
-        <div>
-          <form onSubmit={e => {
-            e.preventDefault()
-            if (!input.value.trim()) {
-              return
-            }
-            addTask(id, input.value)
-            input.value = ''
-          }}>
-            <input ref={node => { input = node }} />
-            <button type='submit'>Add Task</button>
+          <form onSubmit={this.onFormSubmit}>
+            Task Name
+            <FormControl
+              type='text'
+              id='task'
+              value={this.state.task}
+              placeholder='Name of the new task'
+              onChange={this.onInputChange}
+            />
+            Task Description
+            <FormControl
+              type='text'
+              id='description'
+              value={this.state.description}
+              placeholder='Description for the new task'
+              onChange={this.onInputChange}
+            />
+            <Button bsStyle='primary' type='submit'>Create</Button>
           </form>
         </div>
-        <h4 className='to-do'>To Do</h4>
-        {todoList}
         <h4 className='in-progress'>In Progress</h4>
-        {inProgressList}
+          <InProgressList {...this.props} />
+        <h4 className='to-do'>To Do</h4>
+          <ToDoList {...this.props} />
       </div>
     );
   }
 }
 
-Project.propTypes = {
-  addTask: PropTypes.func.isRequired,
-  startTask: PropTypes.func.isRequired,
-  id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  tasks: PropTypes.shape({
-    count: PropTypes.number.isRequired,
-    todo: PropTypes.number.isRequired,
-    inProgress: PropTypes.number.isRequired,
-    list: PropTypes.arrayOf(PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      state: PropTypes.string.isRequired
-    }))
-  })
+function mapStateToProps({ projects }) {
+  return { projects };
 }
 
-export default Project;
+export default connect(mapStateToProps)(Project);
