08d840f2d7382479ef20e662034d49d7d975e40d
Sara <sgee03@gmail.com>
2016-06-25 14:40:15 -0700

    (feat) Add description to reducer

--- src/reducers/projects.js
+++ src/reducers/projects.js
@@ -17,6 +17,7 @@ const task = (state, action) => {
       return {
         id: uuid(),
         name: action.task.name,
+        description: action.task.description,
         state: 'to-do'
       }
     case START_TASK:
